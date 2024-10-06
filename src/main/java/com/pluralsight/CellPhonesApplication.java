997c480b5248f16c4ee32d81bbffe2073bcf389e
Yosh Ntigahera <Yntigahera@gmail.com>
2024-10-06 15:34:36 -0700

    phone info holder, with dialing code

---
+++ src/main/java/com/pluralsight/CellPhonesApplication.java
@@ -0,0 +1,72 @@
+package com.pluralsight;
+
+public class CellPhonesApplication {
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
+
+
+
+
+    /*private int serialNum;
+    private String model;
+    private String carrier;
+    private String phoneNum;
+    private String owner;
+
+    public CellPhonesApplication(){
+        serialNum = 0;
+        model = "";
+        carrier = "";
+        phoneNum = "";
+        owner = "";
+    }
+
+    public int getSerialNum() {
+        return this.serialNum;
+    }
+    public void setSerialNum(int serialNum){
+        this.serialNum = serialNum;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+    public void setModel(String Model){
+        this.model = Model;
+    }
+
+    public String getCarrier() {
+        return this.carrier;
+    }
+    public void setCarrier(String carrier){
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNum() {
+        return this.phoneNum;
+    }
+    public void setPhoneNum(String phoneNum){
+        this.phoneNum = phoneNum;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+    public void setOwner(String owner){
+        this.owner = owner;
+    }
+
+    public String dial(String _phoneNum){
+        return this.owner + "Phone is calling " + _phoneNum;
+
+    }*/
+
+
+
+
+
---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,63 @@
+package com.pluralsight;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        CellPhone phone = new CellPhone();
+
+        System.out.print("Serial #: ");
+       phone.setSerialNum(myScanner.nextInt());
+       myScanner.nextLine();
+
+        System.out.print("model: ");
+        phone.setModel(myScanner.nextLine());
+
+        System.out.print("carrier: ");
+       phone.setCarrier(myScanner.nextLine());
+
+        System.out.print("Phone Number: ");
+        phone.setPhoneNum(myScanner.nextLine());
+
+        System.out.print("Owner: ");
+        phone.setOwner(myScanner.nextLine());
+        myScanner.close();
+
+        System.out.println("costumer info: ");
+
+        System.out.println("Serial number: " + phone.getSerialNum());
+        System.out.println("Model: " + phone.getModel());
+        System.out.println("Carrier: " + phone.getCarrier());
+        System.out.println("Phone Number: " + phone.getPhoneNum());
+        System.out.println("Owner: " + phone.getOwner());
+
+        String call = phone.dial(phone.getPhoneNum());
+
+        System.out.println(call);
+
+
+        CellPhone num1 = new CellPhone();
+        num1.setPhoneNum("602-369-5185");
+        num1.getOwner();
+
+        CellPhone num2 = new CellPhone();
+        num2.setPhoneNum("702-234-2343");
+        num2.setOwner("Lori Harvey");
+
+
+        display(num1);
+        display(num2);
+
+        System.out.println(phone.getOwner() + num1.dial(num2.getPhoneNum()));
+        System.out.println(num2.dial(num1.getPhoneNum()));
+        //System.out.println(num2.dial(num2.getOwner() + num1.getPhoneNum()));
+
+    }
+
+    public static void display(CellPhone phone){
+
+    }
+
+
+}
